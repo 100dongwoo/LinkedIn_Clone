2a1bfbfc4306a52686591bdf62f379cffcb20070
BAEK DONGWOO <qorehddn123@naver.com>
2021-01-02 15:05:38 +0900

    CRA

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyCwGHW5fmPPH4g2mlOS7pxMlpzRF_k3jC0',
+    authDomain: 'linkedin-clone-f97ab.firebaseapp.com',
+    projectId: 'linkedin-clone-f97ab',
+    storageBucket: 'linkedin-clone-f97ab.appspot.com',
+    messagingSenderId: '187847338760',
+    appId: '1:187847338760:web:5368a0c4a1e5105cd45abd',
+};
